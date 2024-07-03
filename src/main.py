23cbb7f3bbc9d67bf502ce37cd6154f258c765da
Artem Volkov <tirydarten@mail.ru>
2024-07-03 19:00:56 +0700

    fixed

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 import re
-import os.path
 
 
 def clear_names(file_name: str) -> list:
     """Функция для очистки имён от лишних символов"""
     new_name_list = list()
-    with open('../data/' + file_name, 'r', encoding="utf-8") as names_file:
+    with open("../data/" + file_name, "r", encoding="utf-8") as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
-            new_name = ''
+            new_name = ""
             for symbol in name_item:
                 if symbol.isalpha():
                     new_name += symbol
@@ -19,7 +18,7 @@ def clear_names(file_name: str) -> list:
 
 def is_cyrillic(name_item: str) -> bool:
     """Проверка на вхождение кириллицы в строку"""
-    return bool(re.search('[а-я А-Я]', name_item))
+    return bool(re.search("[а-я А-Я]", name_item))
 
 
 def filter_russian_names(names_list: list) -> list:
@@ -39,22 +38,17 @@ def filter_english_names(names_list: list) -> list:
             new_names_list.append(name_item)
     return new_names_list
 
+
 def save_to_file(file_name: str, data: str) -> None:
     """Сохраняет данные в файл"""
-    with open('../data/' + file_name, "w", encoding='utf-8') as names_file:
+    with open("../data/" + file_name, "w", encoding="utf-8") as names_file:
         names_file.write(data)
 
 
-if __name__ == '__main__':
-    cleared_name = clear_names('names.txt')
+if __name__ == "__main__":
+    cleared_name = clear_names("names.txt")
 
     filtered_names = filter_russian_names(cleared_name)
-    save_to_file(
-        'russian_names.txt',
-        '\n'.join(sorted(filtered_names))
-    )
+    save_to_file("russian_names.txt", "\n".join(sorted(filtered_names)))
     filtered_names = filter_english_names(cleared_name)
-    save_to_file(
-        'english_names.txt',
-        '\n'.join(sorted(filtered_names))
-    )
+    save_to_file("english_names.txt", "\n".join(sorted(filtered_names)))
